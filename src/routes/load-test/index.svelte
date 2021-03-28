2a6b0b0eff300a6dbe25f77b40b6b951a349b551
NagiC <huoshengchiou@gmail.com>
2021-03-28 23:51:22 +0800

    '[$appUpdate]'

--- src/routes/load-test/index.svelte
+++ src/routes/load-test/index.svelte
@@ -1,10 +1,20 @@
 <!-- a page can export a load function that runs before the component is created. This function runs both during server-side rendering and in the client, and allows you to get data for a page without (for example) showing a loading spinner and fetching data in onMount. -->
+
 <script context="module">
-	console.log('load fn runs');
-	// load is the SvelteKit equivalent of getStaticProps or getServerSideProps in Next.js or asyncData in Nuxt.js.
+	// import fs from "fs/promises"; //node file system //如果在client side執行會fail，因為bowser不支援
+    //  console.log('process.cwd()',process.cwd())
+//     const filePath = path.join(process.cwd(), "data", "dummyBack.json");
+//   //current work directory
+//   const jsonData = await fs.readFile(filePath);
+//   const data = JSON.parse(jsonData);
+
 	// export async function load({ page, fetch, session, context }) {
-	// 	const url = `/blog/${page.params.slug}.json`;
+
+	// 	console.log('load', page, fetch, session, context)
+	// 	const url = `/data/dummyBack.json`;
 	// 	const res = await fetch(url);
+	// 	const payload=await res.json()
+	// 	console.log('payload',payload)
 
 	// 	if (res.ok) {
 	// 		return {
@@ -22,5 +32,21 @@
 </script>
 
 <script>
-	console.log('main script runs');
+	import { amp, browser, dev } from '$app/env';
+	import { goto, prefetch, prefetchRoutes } from '$app/navigation';
+
+	// console.log('main script runs',browser,dev);
+    
+	console.log('goto',goto)
+	
 </script>
+
+<p></p>
+<br>
+
+<!-- <button onClick={()=>goto()}>click</button> -->
+
+
+
+
+
