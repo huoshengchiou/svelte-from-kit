e4c32b0d8d57d1ade63dd4514d63585b7863e8e8
Chiou <Sheng.Chiou@aceraeb.com>
2021-04-09 17:49:36 +0800

    [node-fs]addLoad fn

--- src/routes/node-fs/index.svelte
+++ src/routes/node-fs/index.svelte
@@ -1,3 +1,33 @@
+<script context="module">
+	import { browser } from '$app/env';
+
+	const getFs = async () => {
+		let fs;
+		const module = await import('fs');
+		console.log('fs', module);
+	};
+	if (!browser) {
+		// import path from 'path';
+		getFs();
+	}
+
+	// export const load = async ({ session }) => {
+	// 	//失敗時回狀態，成功時回component用的props
+	// 	if (!session.authenticated) {
+	// 		return {
+	// 			status: 302,
+	// 			redirect: '/auth/unauthorized'
+	// 		};
+	// 	} else {
+	// 		return {
+	// 			props: {
+	// 				email: session.email
+	// 			}
+	// 		};
+	// 	}
+	// };
+</script>
+
 <script>
 	import { onMount } from 'svelte';
 
